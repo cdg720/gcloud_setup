6fcaab92b78e8180f492ac954cc476ca0510c037
Dokook Choe <cdg720@gmail.com>
2020-09-26 10:36:29 -0700

    update scripts 2020/09/26

--- install_docker_ce.sh
+++ install_docker_ce.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Source: https://docs.docker.com/v17.12/install/linux/docker-ce/debian/#install-using-the-repository
+# Source: https://docs.docker.com/engine/install/debian/
 sudo apt-get update
 sudo apt-get install \
      apt-transport-https \
      ca-certificates \
      curl \
-     gnupg2 \
+     gnupg-agent \
      software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
